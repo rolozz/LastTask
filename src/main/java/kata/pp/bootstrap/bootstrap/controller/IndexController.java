85bbbc1cb217b68cb0a852d4cb39fbaa949a8e74
MihailKrasheninnikov <rolerlozz@gmail.com>
2024-05-26 00:12:49 +0300

    commit

--- src/main/java/kata/pp/bootstrap/bootstrap/controller/IndexController.java
+++ src/main/java/kata/pp/bootstrap/bootstrap/controller/IndexController.java
@@ -3,6 +3,7 @@ package kata.pp.bootstrap.bootstrap.controller;
 import kata.pp.bootstrap.bootstrap.model.User;
 import kata.pp.bootstrap.bootstrap.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -15,17 +16,28 @@ import java.security.Principal;
 @RequestMapping("/")
 public class IndexController {
     private final UserService userService;
+    //private final RoleService roleService;
 
     @Autowired
-    public IndexController(UserService userService) {
+    public IndexController(UserService userService ) {
         this.userService = userService;
+        //this.roleService = roleService;
     }
 
     @GetMapping("")
     public String showUsers(Model model, Principal principal) {
         User user = userService.findByUsername(principal.getName());
         model.addAttribute("user", user);
-        model.addAttribute("page", "PAGE_USER");
-        return "user";
+        if (user.getAuthorities()
+                .stream()
+                .map(GrantedAuthority::getAuthority)
+                .toList()
+                .contains("ROLE_ADMIN")) {
+            model.addAttribute("page", "PAGE_ADMIN");
+            return "redirect:/admin";
+        }else  {
+            model.addAttribute("page", "PAGE_USER");
+            return "redirect:/user";}
     }
 }
+
